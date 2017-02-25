e8abf6a0895e3aa81b73fd94240305ff97858d74
David Cramb <david.cramb@gmail.com>
2017-02-25 10:53:33 -0600

    added shuffle function

--- javascript/app.js
+++ javascript/app.js
@@ -1,68 +1,72 @@
-  //1 - 8
-  //2 - 8
-  //3 - 8
-  //4 - 8
-  //5 - 8
-  //6 - 8
-  //7 - 8
-  //8 - 8
-  //9 - 8
-  //10 - 8
-  //11 - 8
-  //12 - 8
-  //W - 8
-  //SK - 4
-  //total 108
-  let deck = [];
-  var build_deck = () => {
-    for (let i = 1; i <= 13; i++) {
-      var cards = [];
-      cards.push(Array(8).fill(i));
-      cards[0].forEach(function(x, index, array){
-        if (x === 13) { 
-          cards[0][index] = {card: {value: i, color: "wild"}}}
-        else {
-          switch (index) {
-          case 0:
-          case 1:
-          cards[0][index] = {card: {value: i, color: "red"}};
-          break;
-          case 2:
-          case 3:
-          cards[0][index] = {card: {value: i, color: "blue"}}
-          break;
-          case 4:
-          case 5:
-          cards[0][index] = {card: {value: i, color: "green"}}
-          break;
-          case 6:
-          case 7:
-          cards[0][index] = {card: {value: i, color: "yellow"}}
-          break;
-          };
-        };
-      });
-      cards.forEach(pushCardsToDeck);
-    };
-        console.log(cards)
-        addSkipCards();
-    };
-
-  var addSkipCards = () => {
+let deck = [];
+let shuffledDeck = [];
+var build_deck = () => {
+  for (let i = 1; i <= 13; i++) {
     var cards = [];
-    for (let i = 0; i < 4; i++){
-      cards.push({card: {value: "skip", color: "none"}});
-    }
-    cards.forEach(function(card){
-      deck.push(card);
+    cards.push(Array(8).fill(i));
+    cards[0].forEach(function(x, index, array){
+      if (x === 13) { 
+        cards[0][index] = {card: {value: i, color: "wild"}}}
+      else {
+        switch (index) {
+        case 0:
+        case 1:
+        cards[0][index] = {card: {value: i, color: "red"}};
+        break;
+        case 2:
+        case 3:
+        cards[0][index] = {card: {value: i, color: "blue"}}
+        break;
+        case 4:
+        case 5:
+        cards[0][index] = {card: {value: i, color: "green"}}
+        break;
+        case 6:
+        case 7:
+        cards[0][index] = {card: {value: i, color: "yellow"}}
+        break;
+        };
+      };
     });
-
-  }  
-  var pushCardsToDeck = (element, index, array) =>
-  {
-    element.forEach(function(card){
-      deck.push(card);
-    });  
+    cards.forEach(pushCardsToDeck);
   };
-  
-  build_deck();
+      console.log(cards)
+      addSkipCards();
+  };
+
+var addSkipCards = () => {
+  var cards = [];
+  for (let i = 0; i < 4; i++){
+    cards.push({card: {value: "skip", color: "none"}});
+  }
+  cards.forEach(function(card){
+    deck.push(card);
+  });
+
+
+
+}  
+var pushCardsToDeck = (element, index, array) =>
+{
+  element.forEach(function(card){
+    deck.push(card);
+  });  
+};
+
+
+build_deck();
+var shuffleNewDeck = (deck, shuffledDeck) => {
+  if (shuffledDeck.length > 0)
+  {
+    shuffledDeck = [];
+  }
+
+//pick random card from deck based on index, if the random number between 0 - 107 has already been picked, skip and try again
+  while (shuffledDeck.length < 108)
+  {
+    let randomNum = Math.floor((Math.random() * 107) + 1)
+    shuffledDeck.push(deck[randomNum]);
+
+  }
+  return shuffledDeck;
+}
